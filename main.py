df34fedae1441ebf50dd2ce11c295493fc3b8d43
TurBoss <j.l.toledano.l@gmail.com>
2017-05-21 05:40:49 +0200

    Customlog

--- main.py
+++ main.py
@@ -4,8 +4,13 @@
 import time
 import sqlite3
 import json
+import logging
 
 from http.server import BaseHTTPRequestHandler, HTTPServer
+from customlog.customlog import ColoredLogger
+
+logging.setLoggerClass(ColoredLogger)
+logger = logging.getLogger('SERVER - MAIN')
 
 db = "db.sqlite"
 
@@ -20,6 +25,7 @@ def dict_factory(cursor, row):
 
 # HTTPRequestHandler class
 class LunaHTTPServer_RequestHandler(BaseHTTPRequestHandler):
+
     # OPTIONS
     def do_OPTIONS(self):
         self.send_response(200, "ok")
@@ -32,13 +38,24 @@ class LunaHTTPServer_RequestHandler(BaseHTTPRequestHandler):
     # GET
 
     def do_GET(self):
+
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
 
-        if self.path == "/check/":
+        if self.path == "/check_gateway/":
             self.wfile.write(bytes("OK", "UTF-8"))
+
+            with sqlite3.connect(db) as conn:
+                conn.row_factory = dict_factory
+                cursor = conn.cursor()
+                cursor.execute("SELECT * FROM gateway")
+                query = cursor.fetchall()
+
+            data = bytes(json.dumps(query), "UTF-8")
+
+            self.wfile.write(data)
             return
 
         return
@@ -54,25 +71,34 @@ class LunaHTTPServer_RequestHandler(BaseHTTPRequestHandler):
 
         # Client
 
-        if self.path == "/client-create/":
-            self.wfile.write(bytes("OK", "UTF-8"))
+        if self.path == "/subscriptor_create/":
+            """
+            var_len = int(self.headers['Content-Length'])
+            json_data = self.rfile.read(var_len)
+            data = json.loads(json_data.decode("UTF-8"))
+
+            with sqlite3.connect(db) as conn:
+                cursor = conn.cursor()
+                cursor.execute("INSERT INTO clients VALUES (NULL, ?, ?, ?, ?, ?)",
+                               (data["name"], data["surname"], data["tlf"], data["email"], data["nif"],))
+                conn.commit()
+            """
             return
         return
 
 
 def main():
-    print('{0} : Server starting'.format(time.asctime()))
 
     # Server settings
     server_address = ('0.0.0.0', 8081)
     httpd = HTTPServer(server_address, LunaHTTPServer_RequestHandler)
 
     try:
+        logger.info('Server starting listening on ({0})'.format(server_address))
         httpd.serve_forever()
     except KeyboardInterrupt as e:
         httpd.server_close()
-        print(e)
-        print('{0} : Server stops'.format(time.asctime()))
+        logger.info('Server stops')
 
 
 if __name__ == "__main__":
