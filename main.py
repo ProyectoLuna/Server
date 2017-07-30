a95dbe2c59d43c8bb648c5d05510ad27828d5430
Miguel Garcia <mgm.mgm.85@gmail.com>
2017-07-30 23:23:26 +0200

    added auto cert creation

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import ssl
 import logging
+from pathlib import Path
 
 from customlog.customlog import ColoredLogger
+from certificate.certificate import create_self_signed_cert
 
 from twisted.web import server
 from twisted.internet import reactor, endpoints, ssl
@@ -13,12 +15,21 @@ def main():
 
     logging.setLoggerClass(ColoredLogger)
     logger = logging.getLogger('SERVER - MAIN')
+    cacert = "cacert.pem"
+    privkey = "privkey.pem"
+
+    # Check if certifice exists
+    cacert_file = Path(cacert)
+    privkey_file = Path(privkey)
+
+    if not cacert_file.is_file() or not privkey_file.is_file():
+        create_self_signed_cert(cacert, privkey)
 
     server_port = 8080
 
     ssl_context = ssl.DefaultOpenSSLContextFactory(
-        'privkey.pem',
-        'cacert.pem',
+        privkey,
+        cacert,
     )
 
     site = server.Site(ServerHandler())
